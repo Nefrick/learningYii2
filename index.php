c2636283c33948f8ee3d644b8d322f5f5d28ed5f
Nefrick <it.nefrick@gmail.com>
2017-03-17 12:50:04 +0200

    OOP

--- index.php
+++ index.php
@@ -16,6 +16,30 @@
 
 </head>
 <body>
-  <h1>Test</h1>
+  <?php
+  class Base
+{
+    public $name = 'Vasya';
+
+    public function first()
+    {
+        return 'first';
+    }
+}
+
+class Sub extends Base
+{
+    public $date = '2000-01-12';
+}
+
+$base = new Base();
+
+echo $base->first() . PHP_EOL;
+
+$sub = new Sub();
+
+echo $sub->first() . PHP_EOL;
+
+?>
 </body>
 </html>
